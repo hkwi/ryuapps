f3cdefe290dc0c7752015ddfc989339a043df32e
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2016-03-07 02:00:06 -0500

    support all ofp versions

--- logstash.py
+++ logstash.py
@@ -52,58 +52,212 @@ class Logstash(ryu.base.app_manager.RyuApp):
 		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		s.connect((self.CONF.logstash.host, self.CONF.logstash.port))
 		
-		def send(unit, req, tm):
-			obj = to_jsondict(unit)
-			obj["datapath_id"] = datapath.id
-			obj["datapath_hex"] = hex(datapath.id)
-			obj["request"] = to_jsondict(req)
-			obj["rtt"] = time.time()-tm
-			s.send(msgpack.dumps(obj))
-		
-		tm = time.time()
+		tcls = {1:V1, 3:V3}.get(datapath.ofproto.OFP_VERSION, Base)
+		tcls(self, s, datapath).collect()
+
+class Base(object):
+	def __init__(self, app, logstash, datapath):
+		self.app = app
+		self.logstash = logstash
+		self.datapath = datapath
+	
+	def collect(self):
+		self.aggregate_stats()
+		self.flow_stats()
+		self.table_stats()
+		self.port_stats()
+		self.queue_stats()
+		self.group_stats()
+		self.meter_stats()
+	
+	def send(self, unit, req, tm):
+		obj = to_jsondict(unit)
+		obj["datapath_id"] = self.datapath.id
+		obj["datapath_hex"] = hex(self.datapath.id)
+		obj["ofp_version"] = self.datapath.ofproto.OFP_VERSION
+		obj["request"] = to_jsondict(req)
+		obj["rtt"] = time.time()-tm
+		self.logstash.send(msgpack.dumps(obj))
+
+	def aggregate_stats(self):
+		datapath = self.datapath
 		p = datapath.ofproto_parser
+		tm = time.time()
 		req = p.OFPAggregateStatsRequest(datapath, 0,
 			datapath.ofproto.OFPTT_ALL,
 			datapath.ofproto.OFPP_ANY,
 			datapath.ofproto.OFPG_ANY, 0, 0,
 			datapath.ofproto_parser.OFPMatch())
-		for msg in api.send_msg(self, req, reply_cls=p.OFPAggregateStatsReply, reply_multi=True):
-			send(msg.body, req, tm)
-		
+		for msg in api.send_msg(self.app, req, reply_cls=p.OFPAggregateStatsReply, reply_multi=True):
+			self.send(msg.body, req, tm)
+
+	def flow_stats(self):
+		datapath = self.datapath
+		p = datapath.ofproto_parser
 		tm = time.time()
 		req = p.OFPFlowStatsRequest(datapath)
-		for msg in api.send_msg(self, req, reply_cls=p.OFPFlowStatsReply, reply_multi=True):
+		for msg in api.send_msg(self.app, req, reply_cls=p.OFPFlowStatsReply, reply_multi=True):
 			for b in msg.body:
-				send(b, req, tm)
-		
+				self.send(b, req, tm)
+
+	def table_stats(self):
+		datapath = self.datapath
+		p = datapath.ofproto_parser
 		tm = time.time()
 		req = p.OFPTableStatsRequest(datapath, 0)
-		for msg in api.send_msg(self, req, reply_cls=p.OFPTableStatsReply, reply_multi=True):
+		for msg in api.send_msg(self.app, req, reply_cls=p.OFPTableStatsReply, reply_multi=True):
 			for b in msg.body:
-				send(b, req, tm)
-		
+				self.send(b, req, tm)
+
+	def port_stats(self):
+		datapath = self.datapath
+		p = datapath.ofproto_parser
 		tm = time.time()
-		req = p.OFPPortStatsRequest(datapath, 0, datapath.ofproto.OFPP_ANY)
-		for msg in api.send_msg(self, req, reply_cls=p.OFPPortStatsReply, reply_multi=True):
+		req = p.OFPPortStatsRequest(datapath, flags=0, port_no=datapath.ofproto.OFPP_ANY)
+		for msg in api.send_msg(self.app, req, reply_cls=p.OFPPortStatsReply, reply_multi=True):
 			for b in msg.body:
-				send(b, req, tm)
-		
+				self.send(b, req, tm)
+
+	def queue_stats(self):
+		datapath = self.datapath
+		p = datapath.ofproto_parser
+		tm = time.time()
+		req = p.OFPQueueStatsRequest(datapath)
+		for msg in api.send_msg(self.app, req, reply_cls=p.OFPQueueStatsReply, reply_multi=True):
+			for b in msg.body:
+				self.send(b, req, tm)
+
+	def group_stats(self):
+		datapath = self.datapath
+		p = datapath.ofproto_parser
+		tm = time.time()
+		req = p.OFPGroupStatsRequest(datapath)
+		for msg in api.send_msg(self.app, req, reply_cls=p.OFPGroupStatsReply, reply_multi=True):
+			for b in msg.body:
+				self.send(b, req, tm)
+
+	def meter_stats(self):
+		datapath = self.datapath
+		p = datapath.ofproto_parser
+		tm = time.time()
+		req = p.OFPMeterStatsRequest(datapath)
+		for msg in api.send_msg(self.app, req, reply_cls=p.OFPMeterStatsReply, reply_multi=True):
+			for b in msg.body:
+				self.send(b, req, tm)
+
+
+class V1(Base):
+	def collect(self):
+		self.aggregate_stats()
+		self.flow_stats()
+		self.table_stats()
+		self.port_stats()
+		self.queue_stats()
+
+	def aggregate_stats(self):
+		datapath = self.datapath
+		p = datapath.ofproto_parser
+		tm = time.time()
+		req = p.OFPAggregateStatsRequest(datapath, 0,
+			datapath.ofproto_parser.OFPMatch(),
+			0xff,
+			datapath.ofproto.OFPP_NONE)
+		for msg in api.send_msg(self.app, req, reply_cls=p.OFPAggregateStatsReply, reply_multi=True):
+			for body in msg.body:
+				self.send(body, req, tm)
+
+	def flow_stats(self):
+		datapath = self.datapath
+		p = datapath.ofproto_parser
+		tm = time.time()
+		req = p.OFPFlowStatsRequest(datapath, 0,
+				datapath.ofproto_parser.OFPMatch(),
+				0xff,
+				datapath.ofproto.OFPP_NONE)
+		for msg in api.send_msg(self.app, req, reply_cls=p.OFPFlowStatsReply, reply_multi=True):
+			for b in msg.body:
+				self.send(b, req, tm)
+
+	def port_stats(self):
+		datapath = self.datapath
+		p = datapath.ofproto_parser
+		tm = time.time()
+		req = p.OFPPortStatsRequest(datapath, 0, datapath.ofproto.OFPP_NONE)
+		for msg in api.send_msg(self.app, req, reply_cls=p.OFPPortStatsReply, reply_multi=True):
+			for b in msg.body:
+				self.send(b, req, tm)
+
+	def queue_stats(self):
+		datapath = self.datapath
+		p = datapath.ofproto_parser
 		tm = time.time()
 		req = p.OFPQueueStatsRequest(datapath, 0,
-			datapath.ofproto.OFPP_ANY,
+			datapath.ofproto.OFPP_ALL,
 			datapath.ofproto.OFPQ_ALL)
-		for msg in api.send_msg(self, req, reply_cls=p.OFPQueueStatsReply, reply_multi=True):
+		for msg in api.send_msg(self.app, req, reply_cls=p.OFPQueueStatsReply, reply_multi=True):
 			for b in msg.body:
-				send(b, req, tm)
-		
+				self.send(b, req, tm)
+
+
+class V3(Base):
+	def collect(self):
+		self.aggregate_stats()
+		self.flow_stats()
+		self.table_stats()
+		self.port_stats()
+		self.queue_stats()
+		self.group_stats()
+	
+	def aggregate_stats(self):
+		datapath = self.datapath
+		p = datapath.ofproto_parser
 		tm = time.time()
-		req = p.OFPGroupStatsRequest(datapath, 0, datapath.ofproto.OFPG_ALL)
-		for msg in api.send_msg(self, req, reply_cls=p.OFPGroupStatsReply, reply_multi=True):
-			for b in msg.body:
-				send(b, req, tm)
-		
+		req = p.OFPAggregateStatsRequest(datapath)
+		for msg in api.send_msg(self.app, req, reply_cls=p.OFPStatsReply, reply_multi=True):
+			self.send(msg.body, req, tm)
+
+	def flow_stats(self):
+		datapath = self.datapath
+		p = datapath.ofproto_parser
 		tm = time.time()
-		req = p.OFPMeterStatsRequest(datapath, 0, datapath.ofproto.OFPM_ALL)
-		for msg in api.send_msg(self, req, reply_cls=p.OFPMeterStatsReply, reply_multi=True):
+		req = p.OFPFlowStatsRequest(datapath)
+		for msg in api.send_msg(self.app, req, reply_cls=p.OFPStatsReply, reply_multi=True):
 			for b in msg.body:
-				send(b, req, tm)
+				self.send(b, req, tm)
+
+	def table_stats(self):
+		datapath = self.datapath
+		p = datapath.ofproto_parser
+		tm = time.time()
+		req = p.OFPTableStatsRequest(datapath, 0)
+		for msg in api.send_msg(self.app, req, reply_cls=p.OFPStatsReply, reply_multi=True):
+			for b in msg.body:
+				self.send(b, req, tm)
+
+	def port_stats(self):
+		datapath = self.datapath
+		p = datapath.ofproto_parser
+		tm = time.time()
+		req = p.OFPPortStatsRequest(datapath)
+		for msg in api.send_msg(self.app, req, reply_cls=p.OFPStatsReply, reply_multi=True):
+			for b in msg.body:
+				self.send(b, req, tm)
+
+	def queue_stats(self):
+		datapath = self.datapath
+		p = datapath.ofproto_parser
+		tm = time.time()
+		req = p.OFPQueueStatsRequest(datapath)
+		for msg in api.send_msg(self.app, req, reply_cls=p.OFPStatsReply, reply_multi=True):
+			for b in msg.body:
+				self.send(b, req, tm)
+
+	def group_stats(self):
+		datapath = self.datapath
+		p = datapath.ofproto_parser
+		tm = time.time()
+		req = p.OFPGroupStatsRequest(datapath)
+		for msg in api.send_msg(self.app, req, reply_cls=p.OFPStatsReply, reply_multi=True):
+			for b in msg.body:
+				self.send(b, req, tm)
+
