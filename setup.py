b6a65d49920cf51a38eaf9a691d16daaaf3f75eb
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2016-03-10 21:25:59 +0900

    enable packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+try:
+	from setuptools import setup
+except:
+	from distutils.core import setup
+
+setup(name="ryuapps",
+	version="0.1",
+	description="Openflow ryu utility apps",
+	author="Hiroaki KAWAI",
+	author_email="hiroaki.kawai@gmail.com",
+	url="https://github.com/hkwi/ryuapps",
+	packages=["ryu_logstash", "ryu_rproxy"])
